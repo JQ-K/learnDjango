9421adfed2435ce709bc307926c4e19ff24e95d9
JQ-K <kangqingjie01@gmail.com>
2018-07-07 18:50:06 +0800

    for 循环

--- test4/booktest/views.py
+++ test4/booktest/views.py
@@ -3,7 +3,9 @@ from models import *
 # Create your views here.
 
 def index(request):
-    hero = HeroInfo.objects.get(pk=1)
-    context = {'hero':hero}
+    #hero = HeroInfo.objects.get(pk=1)
+    #context = {'hero':hero}
+    list = HeroInfo.objects.filter(isDelete=False)
+    context = {'list': list}
     return render(request,'index.html', context)
 
