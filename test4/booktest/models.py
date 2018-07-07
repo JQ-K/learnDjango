4cb20b9dbbc70c983b0fb48629d41376de52f428
JQ-K <kangqingjie01@gmail.com>
2018-07-07 14:30:07 +0800

    wtite model

--- test4/booktest/models.py
+++ test4/booktest/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField(db_column='pub_date')
+    bread = models.IntegerField()
+    bcomment = models.IntegerField()
+    isDelete = models.BooleanField()
+    class Meto():
+        db_table='bookinfo'
+
+class Hero_info(models.Model):
+    hname = models.CharField(max_length=10)
+    hgender = models.BooleanField()
+    hcontent = models.TextField()
+    isDelete = models.BooleanField()
+    book = models.ForeingKey('BookInfo')
+
+    
+
+
+
