9abbb9b4b85d45928e0a0b134862648b453eaeaa
JQ-K <kangqingjie01@gmail.com>
2018-07-10 18:05:56 +0800

    design model and migrate

--- webstore/df_user/models.py
+++ webstore/df_user/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=40)
+    uemail = models.CharField(max_length=30)
+    ushou = models.CharField(max_length=20)
+    uaddress = models.CharField(max_length=100)
+    uyoubian=models.CharField(max_length=6)
+    uphone = models.CharField(max_length=11)
+
